1b7f04d6765c6bd6b89cac515700ecf54d1c7e48
Leo <ductien.tran@jobhopin.com>
2021-06-16 09:34:25 +0700

    commit

--- src/test/java/org/selenium4/GeoLocationTest.java
+++ src/test/java/org/selenium4/GeoLocationTest.java
@@ -12,7 +12,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class GeoLocationTest {
     protected static ChromeDriver driver;
-    String URL = "https://locations.dennys.com/search.html/";
+    String URL = "https://maps.google.com/";
 
     @BeforeClass
     public void testSetUp() {
@@ -25,14 +25,22 @@ public class GeoLocationTest {
     public void test_Selenium4_Geolocation() throws InterruptedException {
         Map< String,Object> coordinates =
                 new HashMap< String,Object>();
+        Map< String,Object> coordinates1 =
+                new HashMap< String,Object>();
 
         /* Create a hashmap for latitude, longitude, and accuracy as needed by Google Maps */
-        coordinates.put("latitude", 42.1408845);
-        coordinates.put("longitude", -72.5033907);
-        coordinates.put("accuracy", 100);
+        coordinates1.put("latitude", 10.78336657835731);
+        coordinates1.put("longitude", 106.70208992062993);
+        coordinates1.put("accuracy", 100);
+
+
+        coordinates1.put("latitude", 10.7756037658568911);
+        coordinates1.put("longitude", 106.70017143655485);
+        coordinates1.put("accuracy", 100);
 
         /* Command to emulate Geolocation */
         driver.executeCdpCommand("Emulation.setGeolocationOverride", coordinates);
+        driver.executeCdpCommand("Emulation.setGeolocationOverride", coordinates1);
         driver.navigate().to(URL);
         driver.manage().window().maximize();
 
@@ -40,9 +48,9 @@ public class GeoLocationTest {
         /* Not a good programming practice, added for demonstration */
         Thread.sleep(5000);
 
-        WebElement location_icon = driver.findElement(By.cssSelector(".icon-geolocate"));
+        //WebElement location_icon = driver.findElement(By.cssSelector(".icon-geolocate"));
         Thread.sleep(2000);
-        location_icon.click();
+        //location_icon.click();
 
         Thread.sleep(6000);
         System.out.println("Geolocation testing with Selenium is complete");
